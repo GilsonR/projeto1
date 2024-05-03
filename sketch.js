099fa1529182e0a1ffefa838faca838ae4b7ba28
Gilson <150448232+GilsonR@users.noreply.github.com>
2024-05-03 11:12:50 -0300

    criei o arquivo sketch.js

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let cor;
+let posicaoHorizontal; //x
+let posicaoVertical; //y
+
+function setup() {
+  createCanvas(400, 400);
+  random(0,255);
+  background(220);
+  cor = color(random(0,255),random(0,255),random(0,255));
+  posicaoHorizontal= 100; //x
+  posicaoVertical= 100;//y
+}
+
+function draw() {
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,50);
+  
+  if(mouseX < posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal - 1;
+  }
+  if(mouseX > posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal + 1;
+  }
+  if(mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+  if(mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+  if(mouseIsPressed){
+     cor = color(random(0,255),random(0,255),random(0,255),random(0,255));
+  }
+
+}
